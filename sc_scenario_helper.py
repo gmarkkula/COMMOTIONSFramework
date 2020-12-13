5ecd514d0381abe67f5f8f5fef3d3de90bcb618a
gmarkkula <g.markkula@leeds.ac.uk>
2020-12-13 09:51:42 +0000

    Update sc_scenario_helper.py
    Adding more "unit tests" and fixing some special cases identified in the process.

--- sc_scenario_helper.py
+++ sc_scenario_helper.py
@@ -4,21 +4,42 @@ from enum import Enum
 
 import commotions
 
+EPSILON = np.finfo(float).eps
 
 class CtrlType(Enum):
     SPEED = 0
     ACCELERATION = 1
 
+
 def get_acc_to_be_at_dist_at_time(speed, target_dist, target_time):
+    """ Return acceleration required to travel a further distance target_dist
+        in time target_time if starting at speed speed. Handle infinite
+        target_time by returning machine epsilon with correct sign.
+    """
     
     assert target_time > 0    
     
-    # general answer
-    target_acc = 2 * (target_dist - speed * target_time) / (target_time ** 2)
+    # time horizon finite or not?
+    if target_time == math.inf:
+        # infinite time horizon - some special cases with near-zero acceleration
+        if target_dist == 0:
+            # at target, so just reverse any speed
+            return -np.sign(speed) * EPSILON
+        elif speed == 0:
+            # at zero speed, so remove remaining distance
+            return np.sign(target_dist) * EPSILON
+        else:  
+            # neither distance nor speed are zero, so sign of acceleration
+            # depends on combination of both - and we need to do the stopping 
+            # check below, so can't return straight away
+            target_acc = -np.sign(speed * target_dist) * EPSILON
+    else:
+        # finite time horizon --> general answer for most situations
+        target_acc = 2 * (target_dist - speed * target_time) / (target_time ** 2)
     
-    # we have to check whether this is in fact a stopping situation - if so
-    # should apply stopping deceleration instead (to avoid passing beyond 
-    # target distance and reversing back toward it again)
+    # check whether this is a stopping situation - if so we should apply 
+    # stopping deceleration instead (to avoid passing beyond target distance 
+    # and reversing back toward it again)
     if target_dist > 0 and speed > 0 and target_acc < 0:
         stop_acc = -speed ** 2 / (2 * target_dist)
         stop_time = speed / (-stop_acc)
@@ -26,11 +47,24 @@ def get_acc_to_be_at_dist_at_time(speed, target_dist, target_time):
             # stopping time is shorter than target time, so we will need to
             # stop at target and wait
             return stop_acc
-        
-    # in all other cases the general answer above should be the correct solution
+    
+    # not a stopping situation, so return what was calculated above
     return target_acc
         
-        
+
+def get_entry_exit_times(d, v, coll_dist):
+    """ Return tuple of entry and exit times into conflict zone defined by +/-coll_dist
+        around conflict point, for agent currently at distance d to conflict 
+        point and travelling toward it at speed v. Returning inf for zero speeds,
+        but not checking for negative distances or speeds.
+    """
+    if v == 0:
+        return (math.inf, math.inf)
+    else:
+        entry_time = (d - coll_dist) / v
+        exit_time = (d + coll_dist) / v
+        return (entry_time, exit_time)
+
 
 def get_access_order_accs(ego_ctrl_type, ego_action_dur, ego_k, ego_v_free,
                           ego_d, ego_v, oth_d, oth_v, coll_dist):
@@ -54,6 +88,9 @@ def get_access_order_accs(ego_ctrl_type, ego_action_dur, ego_k, ego_v_free,
         # yes, so nothing to do here
         return (math.nan, math.nan)
     
+    # get other agent's entry/exit times into to the conflict space
+    (oth_entry_time, oth_exit_time) = get_entry_exit_times(oth_d, oth_v, coll_dist)
+    
     # get ego agent's acceleration if just aiming for free speed
     if ego_ctrl_type is CtrlType.SPEED:
         # assuming straight acc to free speed
@@ -76,7 +113,6 @@ def get_access_order_accs(ego_ctrl_type, ego_action_dur, ego_k, ego_v_free,
         # no, so it is theoretically possible to pass in front of it
         # --> get acceleration that has the ego agent be at exit of the conflict 
         # space at the same time as the other agent enters it
-        oth_entry_time = (oth_d - coll_dist) / oth_v
         ego_dist_to_exit = ego_d + coll_dist
         ego_acc_1st = get_acc_to_be_at_dist_at_time(ego_v, ego_dist_to_exit,
                                                     oth_entry_time)
@@ -90,7 +126,6 @@ def get_access_order_accs(ego_ctrl_type, ego_action_dur, ego_k, ego_v_free,
     if ego_d <= coll_dist:
         # yes, so need to move back out of it again before the other agent 
         # arrives
-        oth_entry_time = (oth_d - coll_dist) / oth_v
         ego_dist_to_entry = ego_d - coll_dist
         ego_acc_2nd = get_acc_to_be_at_dist_at_time(ego_v, ego_dist_to_entry,
                                                     oth_entry_time)
@@ -98,7 +133,6 @@ def get_access_order_accs(ego_ctrl_type, ego_action_dur, ego_k, ego_v_free,
         # not yet reached conflict space, so get acceleration that has the ego 
         # agent be at entrance to the conflict space at the same time as the 
         # other agent exits it
-        oth_exit_time = (oth_d + coll_dist) / oth_v
         ego_dist_to_entry = ego_d - coll_dist
         ego_acc_2nd = get_acc_to_be_at_dist_at_time(ego_v, ego_dist_to_entry,
                                                     oth_exit_time)
@@ -111,7 +145,7 @@ def get_access_order_accs(ego_ctrl_type, ego_action_dur, ego_k, ego_v_free,
         
         
 
-# code for testing the helper functions
+# "unit tests"
 
 if __name__ == "__main__":
     
@@ -120,62 +154,104 @@ if __name__ == "__main__":
     
     plt.close('all')
     
+
+# =============================================================================
+#   Testing get_access_order_accs()
+# =============================================================================
+
+    # defining the tests
+    TESTS = ('Stationary ped., moving veh.', 'Moving ped., moving veh.', 
+             'Stationary veh., moving ped.', 'Moving veh., moving ped.', 
+             'Moving veh., stationary ped.', 'Stationary veh., stationary ped.')
+    N_TESTS = len(TESTS)
+    EGO_CTRL_TYPES = (CtrlType.SPEED, CtrlType.SPEED, CtrlType.ACCELERATION, 
+                      CtrlType.ACCELERATION, CtrlType.ACCELERATION, 
+                      CtrlType.ACCELERATION)
+    EGO_VS = (0, 1.5, 0, 10, 10, 0)
+    OTH_VS = (10, 10, 1.5, 1.5, 0, 0)
     
-    ## TODO : Test case with acceleration-controlling agent
-    
-    
-    
-    EGO_CTRL_TYPE = CtrlType.SPEED
+    # constants depending on ego agent control type
+    EGO_D_MAX = {}
+    EGO_D_MAX[CtrlType.SPEED] = 40
+    EGO_D_MAX[CtrlType.ACCELERATION] = 100
+    EGO_V_FREE = {}
+    EGO_V_FREE[CtrlType.SPEED] = 1.5
+    EGO_V_FREE[CtrlType.ACCELERATION] = 10
+    EGO_K = {}
+    EGO_K[CtrlType.SPEED] = commotions.Parameters() # not used
+    EGO_K[CtrlType.ACCELERATION] = commotions.Parameters()
+    EGO_K[CtrlType.ACCELERATION]._g = 1 
+    EGO_K[CtrlType.ACCELERATION]._dv = 0.05
+    EGO_K[CtrlType.ACCELERATION]._da = 0.01
+    OTH_D = {}
+    OTH_D[CtrlType.SPEED] = 40
+    OTH_D[CtrlType.ACCELERATION] = 6
+    # other constants
     EGO_ACTION_DUR = 0.5
-    EGO_K = commotions.Parameters()
-    EGO_V_FREE = 1.5
-    EGO_Ds = np.arange(40, -5, -1)
-    EGO_V = 1.5
-    OTH_D = 40
-    OTH_V = 10
     COLL_DIST = 2
-    
     END_TIME = 10 # s
     TIME_STEP = 0.005 # s
     TIME_STAMPS = np.arange(0, END_TIME, TIME_STEP)
     
-    # get other agent's movement
-    oth_distances = OTH_D - OTH_V * TIME_STAMPS
-    oth_entry_time = (OTH_D - COLL_DIST) / OTH_V
-    oth_exit_time = (OTH_D + COLL_DIST) / OTH_V
-    
-    def plot_conflict_window(ax):
+    # plot fcns
+    def plot_conflict_window(ax, oth_entry_time, oth_exit_time):
         ax.axhline(COLL_DIST, color='r', linestyle='--', lw=0.5)
         ax.axhline(-COLL_DIST, color='r', linestyle='--', lw=0.5)
         ax.axvline(oth_entry_time, color='r', linestyle='--', lw=0.5)
         ax.axvline(oth_exit_time, color='r', linestyle='--', lw=0.5)
-        
-    # plot other agent's path
-    fig = plt.figure('Other agent')
-    ax = fig.subplots()
-    ax.plot(TIME_STAMPS, oth_distances, 'k-')
-    plot_conflict_window(ax)
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Distance to conflict point (m)')
-    
-    fig_1st = plt.figure('Passing first')
-    ax_1st = fig_1st.subplots()
-    plot_conflict_window(ax_1st)
-    fig_2nd = plt.figure('Passing second')
-    ax_2nd = fig_2nd.subplots()
-    plot_conflict_window(ax_2nd)
-    
     def plot_pred_ds(ax, d, v, a):
         vs = v + a * TIME_STAMPS
         ds = d - np.cumsum(vs * TIME_STEP)
         ax.plot(TIME_STAMPS, ds, 'k-', lw=0.5)
+    
+    # loop through the test cases
+    for i_test, test_name in enumerate(TESTS):
         
-    for ego_d in EGO_Ds:
-        (acc_1st, acc_2nd) = get_access_order_accs(
-                EGO_CTRL_TYPE, EGO_ACTION_DUR, EGO_K, EGO_V_FREE,
-                ego_d, EGO_V, OTH_D, OTH_V, COLL_DIST)
-        plot_pred_ds(ax_1st, ego_d, EGO_V, acc_1st)
-        plot_pred_ds(ax_2nd, ego_d, EGO_V, acc_2nd)
+        # get test settings
+        ego_ctrl_type = EGO_CTRL_TYPES[i_test]
+        ego_v = EGO_VS[i_test]
+        oth_v = OTH_VS[i_test]
+        ego_ds = np.linspace(-5, EGO_D_MAX[ego_ctrl_type], 20)
+        ego_v_free = EGO_V_FREE[ego_ctrl_type]
+        ego_k = EGO_K[ego_ctrl_type]
+        oth_d = OTH_D[ego_ctrl_type]
+        
+        # get other agent's movement
+        oth_distances = oth_d - oth_v * TIME_STAMPS
+        (oth_entry_time, oth_exit_time) = get_entry_exit_times(
+                oth_d, oth_v, COLL_DIST)
+            
+        # get figure window for test results
+        fig = plt.figure(test_name, figsize = (10, 5))
+        axs = fig.subplots(1, 3)
+        
+        # plot other agent's path
+        axs[0].plot(TIME_STAMPS, oth_distances, 'k-')
+        plot_conflict_window(axs[0], oth_entry_time, oth_exit_time)
+        axs[0].set_xlabel('Time (s)')
+        axs[0].set_ylabel('$d_{oth}$ (m)')
+        axs[0].set_title("Other agent")
+        
+        # prepare subplots for passing first/second plots
+        axs[1].set_xlabel('Time (s)')
+        axs[1].set_ylabel('$d_{ego}$ (m)')
+        axs[1].set_title('Ego agent passing first')
+        plot_conflict_window(axs[1], oth_entry_time, oth_exit_time)
+        axs[2].set_xlabel('Time (s)')
+        axs[2].set_ylabel('$d_{ego}$ (m)')
+        axs[2].set_title('Ego agent passing second')
+        plot_conflict_window(axs[2], oth_entry_time, oth_exit_time)
+            
+        # loop through initial distances
+        for ego_d in ego_ds:
+            (acc_1st, acc_2nd) = get_access_order_accs(
+                    ego_ctrl_type, EGO_ACTION_DUR, ego_k, ego_v_free,
+                    ego_d, ego_v, oth_d, oth_v, COLL_DIST)
+            plot_pred_ds(axs[1], ego_d, ego_v, acc_1st)
+            plot_pred_ds(axs[2], ego_d, ego_v, acc_2nd)
+            
+        plt.tight_layout()
+        plt.show()
     
     
     
