579553c35e581f2570412e146b93d659d870de48
gmarkkula <g.markkula@leeds.ac.uk>
2021-09-22 11:27:21 +0100

    Update parameter_search.py
    Adding functionality for handling status message output with different degrees of verbosity from the ParameterSearch class.

--- SCPaper/parameter_search.py
+++ SCPaper/parameter_search.py
@@ -6,8 +6,10 @@ Created on Sat Sep 18 09:26:51 2021
 """
 import math
 import numpy as np
+import datetime
 import pickle
 
+STATUS_REP_HEADER_LEN = 25
 
 class ParameterSearchResults:
     def __init__(self, n_params, n_metrics, n_parameterisations, 
@@ -22,6 +24,55 @@ class ParameterSearchResults:
 
 
 class ParameterSearch:
+    
+    def verbosity_push(self):
+        """
+        To be called by descendant classes to indicate an increase in depth
+        of processing.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.curr_verbosity_depth += 1
+    
+    def verbosity_pop(self):
+        """
+        To be called by descendant classes to indicate a decrease in depth
+        of processing.
+
+        Returns
+        -------
+        None.
+        """
+        self.curr_verbosity_depth = max(0, self.curr_verbosity_depth - 1)
+        
+    def verbose_now(self):
+        """
+        Indicates whether the user has requested status output at the current 
+        depth of processing.
+
+        Returns
+        -------
+        Boolean
+            True if the current depth of processing is less than or equal to
+            the user-specified threshold.
+
+        """
+        return (self.curr_verbosity_depth <= self.max_verbosity_depth)
+    
+    def report(self, message):
+        if self.verbose_now():
+            header = datetime.datetime.now().strftime('%x %X') + ' ' + self.name
+            if len(header) > STATUS_REP_HEADER_LEN:
+                header = header[:STATUS_REP_HEADER_LEN]
+            elif len(header) < STATUS_REP_HEADER_LEN:
+                header = header + ' ' * (STATUS_REP_HEADER_LEN - len(header))
+            padding = '--' * (self.curr_verbosity_depth-1)
+            print(header + ' ' + padding + ' ' + message)
+                
+        
     
     def get_params_dict(self, params_array):
         """
@@ -96,6 +147,7 @@ class ParameterSearch:
         None.
 
         """
+        self.verbosity_push()
         shape = params_matrix.shape
         assert(shape[1] == self.n_params)
         n_parameterisations = shape[0]
@@ -103,9 +155,13 @@ class ParameterSearch:
                                               n_metrics = self.n_metrics,
                                               n_parameterisations = n_parameterisations,
                                               params_matrix = params_matrix)
+        self.report(f'Searching {n_parameterisations} parameterisations for'
+                    f' parameters {self.param_names}...')
         for i in range(n_parameterisations):
             self.results.metrics_matrix[i, :] = self.get_metrics_for_params(
                 self.results.params_matrix[i, :])
+        self.report('Parameter search complete.')
+        self.verbosity_pop()
             
     
     def search_grid(self, param_arrays):
@@ -143,18 +199,19 @@ class ParameterSearch:
                     param_arrays[self.param_names[i_param]][i_param_val_pos]
         # search the matrix of parameterisations
         self.search_list(params_matrix)
+        
     
-    def save(self, file_name, verbose=False):
-        if verbose:
-            print(f'Saving results of "{self.name}" to file "{file_name}"...')
+    def save(self, file_name):
+        self.verbosity_push()
+        self.report(f'Saving results of "{self.name}" to file "{file_name}"...')
         file_obj = open(file_name, 'wb')
         pickle.dump(self, file_obj)
         file_obj.close()
-        if verbose:
-            print('\tDone.')
+        self.report('\tSaving done.')
+        self.verbosity_pop()
         
     
-    def __init__(self, param_names, metric_names, name='Unnamed'):
+    def __init__(self, param_names, metric_names, name='Unnamed', verbosity=0):
         """
         Constructor.
 
@@ -166,6 +223,9 @@ class ParameterSearch:
             Names of the metrics to be calculated for each parameterisation.
         name : string, optional
             Name for the parameter search. The default is 'Unnamed'.
+        verbosity : int, optional
+            The deepest level of processing at which to provide status report
+            information. The default is 0, i.e., no status reports at all.
 
         Returns
         -------
@@ -177,6 +237,8 @@ class ParameterSearch:
         self.param_names = param_names
         self.n_metrics = len(metric_names)
         self.metric_names = metric_names
+        self.max_verbosity_depth = verbosity
+        self.curr_verbosity_depth = 0
         
 
 def load(file_name, verbose=False):
@@ -203,12 +265,13 @@ if __name__ == "__main__":
             metrics['m1'] = params['p1'] + params['p2'] + params['p3']
             metrics['m2'] = -metrics['m1']
             return self.get_metrics_array(metrics)
-        def __init__(self, name):
+        def __init__(self, name, verbosity):
             super().__init__(param_names=('p1', 'p2', 'p3'),
-                             metric_names=('m1', 'm2'), name=name)
+                             metric_names=('m1', 'm2'), name=name,
+                             verbosity=verbosity)
     
     # test list search        
-    test_search = TestParameterSearch(name='test1')
+    test_search = TestParameterSearch(name='test1', verbosity=1)
     params_matrix = np.array((
         (0, 0, 0),
         (1, 1, 1),
@@ -220,14 +283,14 @@ if __name__ == "__main__":
     
     # test saving and loading
     file_name = '_delme/search_save_test.pkl'
-    test_search.save(file_name, verbose=True)
+    test_search.save(file_name)
     reloaded_search = load(file_name, verbose=True)
     results = np.concatenate((reloaded_search.results.params_matrix, 
                reloaded_search.results.metrics_matrix), axis=1)
     print(results)
     
     # test grid search
-    test_search = TestParameterSearch(name='test2')
+    test_search = TestParameterSearch(name='test2', verbosity=0)
     param_arrays = {}
     param_arrays['p1'] = (0,1,2,3)
     param_arrays['p2'] = np.arange(0, 9, 3)
