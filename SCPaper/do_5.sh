9dfba8e241d8211ff1303dbc02b9adb3e93b44df
gmarkkula <g.markkula@leeds.ac.uk>
2022-01-19 14:29:29 +0000

    Adding ARC4 bash/qsub scripts for do_5 and do_8

---
+++ SCPaper/do_5.sh
@@ -0,0 +1,18 @@
+
+#Run with current environment (-V) and in the current directory (-cwd)
+#$ -V -cwd
+
+#Request some time- min 15 mins - max 48 hours
+#$ -l h_rt=01:30:00
+
+#Request cores
+#$ -pe smp 40
+
+#Get email at start and end of the job
+#$ -m be
+
+#Now run the job
+module load anaconda
+source activate base
+python do_5_probabilistic_fitting.py
+
---
+++ SCPaper/do_8.sh
@@ -0,0 +1,19 @@
+
+#Run with current environment (-V) and in the current directory (-cwd)
+#$ -V -cwd
+
+#Request some time- min 15 mins - max 48 hours
+#$ -l h_rt=01:00:00
+
+#Request cores
+#$ -pe smp 40
+
+#Get email at start and end of the job
+#$ -m be
+
+#Now run the job
+module load anaconda
+source activate base
+python do_8_combined_fitting.py
+
+
