91cca1bbaa57e40a53f1c16a535beec4c058c461
gmarkkula <g.markkula@leeds.ac.uk>
2022-01-28 07:55:14 +0000

    ARC4 HIKER fits

--- SCPaper/do_12_analyse_HIKER_fits.py
+++ SCPaper/do_12_analyse_HIKER_fits.py
@@ -20,6 +20,13 @@ import numpy as np
 import parameter_search
 import sc_fitting
 
+
+MAX_NON_CROSS_TRIALS = 60
+if not np.isinf(MAX_NON_CROSS_TRIALS):
+    print(f'NB: Excluding parameterisations with more than {MAX_NON_CROSS_TRIALS}'
+          ' non-crossing trials.\n')
+
+
 # find pickle files from the HIKER fits
 hiker_fit_files = glob.glob(sc_fitting.FIT_RESULTS_FOLDER + 
                             (sc_fitting.HIKER_FIT_FILE_NAME_FMT % '*'))
@@ -28,31 +35,43 @@ print(hiker_fit_files)
 
 
 # loop through the fitting results files
+n_total_params_tested = 0
 for hiker_fit_file in hiker_fit_files:
     print()
     hiker_fit = parameter_search.load(hiker_fit_file, verbose=True)
+    print(f'\tParameterisations tested: {hiker_fit.n_parameterisations}')
+    n_total_params_tested += hiker_fit.n_parameterisations
     
-    # reorganise data for plotting/analysis
-    model_cits = {}
-    for i_speed, veh_speed_mph in enumerate(sc_fitting.HIKER_VEH_SPEEDS_MPH):
-        for i_gap, veh_time_gap in enumerate(sc_fitting.HIKER_VEH_TIME_GAPS):
-            for i_yield, veh_yielding in enumerate((False, True)):
-                scen_name = sc_fitting.get_hiker_scen_name(veh_speed_mph, 
-                                                           veh_time_gap, 
-                                                           veh_yielding)
-                i_scen = hiker_fit.metric_names.index(
-                    scen_name + '_' + sc_fitting.HIKER_CIT_METRIC_NAME)
-                model_cits[scen_name] = pd.DataFrame(columns=('i_param', 
-                                                              'crossing_time'))
-                for i_param in range(hiker_fit.n_parameterisations):
-                    param_cits = pd.DataFrame(
-                        {'i_param': np.full(hiker_fit.n_repetitions, i_param),
-                         'crossing_time': hiker_fit.results.metrics_matrix[
-                             i_param, i_scen, :]})
-                    model_cits[scen_name] = model_cits[scen_name].append(param_cits)
+    # go through
+    i_included_params = []
+    for i_param in range(hiker_fit.n_parameterisations):
+        if np.count_nonzero(np.isnan(hiker_fit.results.metrics_matrix[i_param, :, :])) <= MAX_NON_CROSS_TRIALS:
+            i_included_params.append(i_param)
     
-    
-    sc_fitting.do_hiker_cit_cdf_plot(model_cits, fig_name=hiker_fit.name)
-                    
+    if len(i_included_params) > 0:
+        print(f'Plotting for {len(i_included_params)} included parameterisations:')
+        # reorganise data for plotting/analysis
+        model_cits = {}
+        for i_speed, veh_speed_mph in enumerate(sc_fitting.HIKER_VEH_SPEEDS_MPH):
+            for i_gap, veh_time_gap in enumerate(sc_fitting.HIKER_VEH_TIME_GAPS):
+                for i_yield, veh_yielding in enumerate((False, True)):
+                    scen_name = sc_fitting.get_hiker_scen_name(veh_speed_mph, 
+                                                               veh_time_gap, 
+                                                               veh_yielding)
+                    i_scen = hiker_fit.metric_names.index(
+                        scen_name + '_' + sc_fitting.HIKER_CIT_METRIC_NAME)
+                    model_cits[scen_name] = pd.DataFrame(columns=('i_param', 
+                                                                  'crossing_time'))
+                    for i_param in i_included_params:
+                        param_cits = pd.DataFrame(
+                            {'i_param': np.full(hiker_fit.n_repetitions, i_param),
+                             'crossing_time': hiker_fit.results.metrics_matrix[
+                                 i_param, i_scen, :]})
+                        model_cits[scen_name] = model_cits[scen_name].append(param_cits)
+        
+        
+        sc_fitting.do_hiker_cit_cdf_plot(model_cits, fig_name=hiker_fit.name)
+
+print(f'*** Total number of parameterisations tested: {n_total_params_tested}')                 
                 
                 
