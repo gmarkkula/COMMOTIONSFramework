95f0028d047d383a294e04f5a7c6e6c812764076
gmarkkula <g.markkula@leeds.ac.uk>
2022-02-25 17:19:40 +0000

    Update do_fig1.py

--- SCPaper/do_fig1.py
+++ SCPaper/do_fig1.py
@@ -20,10 +20,11 @@ import do_2_analyse_deterministic_fits
 from do_2_analyse_deterministic_fits import get_max_crit_parameterisations, get_best_scen_var_for_paramet
 
 
-OVERWRITE_SAVED_SIM_RESULTS = False
 PLOT_ILLUSTRATIONS = True
 PLOT_METRICS = True
 PLOT_METRIC_VALUES = True
+PLOT_DET_EXAMPLES = True
+OVERWRITE_SAVED_DET_SIM_RESULTS = False
 SAVE_PDF = False
 
 DPI = sc_plot.DPI / 3
@@ -47,14 +48,14 @@ DET_SIM_RESULTS_FNAME = 'fig_1_DetFitSimResults.pkl'
 rng = default_rng(0)
 
 plt.close('all')
-fig, axs = plt.subplots(num=1, nrows = N_ROWS, ncols = N_COLS, 
+fig, fig_axs = plt.subplots(num=1, nrows = N_ROWS, ncols = N_COLS, 
                         figsize=(sc_plot.FULL_WIDTH, 0.8*sc_plot.FULL_WIDTH),
                         dpi=DPI, tight_layout=True)
 
 if PLOT_ILLUSTRATIONS:
     print('Plotting illustrations...')
     for i_scenario, scenario in enumerate(SCENARIO_NAMES):
-        ax = axs[0, i_scenario]
+        ax = fig_axs[0, i_scenario]
         scen_im = plt.imread(sc_plot.FIGS_FOLDER + scenario + '.png')
         ax.imshow(scen_im)
         ax.axis('off')
@@ -67,17 +68,17 @@ with open(os.devnull, 'w') as devnull:
         do_2_analyse_deterministic_fits.SAVE_RETAINED_MODELS = False
         det_fits = do_2_analyse_deterministic_fits.do()
         
+    
+CRIT_BASES = ('', 'oVA')
+BASE_DASHES = ((10,2), (1,0))
+CRIT_VARIANTS = ('', 'oBEo', 'oBEvoAI')
+VARIANT_COLORS = (sc_plot.COLORS['base variant black'],
+                  sc_plot.COLORS['oBEo variant yellow'],
+                  'green')
         
 if PLOT_METRICS:
     
     print('Plotting metric distributions...')
-    
-    CRIT_BASES = ('', 'oVA')
-    BASE_DASHES = ((10,2), (1,0))
-    CRIT_VARIANTS = ('', 'oBEo', 'oBEvoAI')
-    VARIANT_COLORS = (sc_plot.COLORS['base variant black'],
-                      sc_plot.COLORS['oBEo variant yellow'],
-                      'green')
     
     def transf_kde(x):
         kde_y = kde(x) * yscale
@@ -89,7 +90,7 @@ if PLOT_METRICS:
         xrange = xlims[1] - xlims[0]
         yscale = xrange/70
         for i_base, base in enumerate(CRIT_BASES):
-            ax = axs[i_base + 1, i_scenario]
+            ax = fig_axs[i_base + 1, i_scenario]
             for i_variant, variant in enumerate(CRIT_VARIANTS):
                 base_y = len(CRIT_VARIANTS) - i_variant
                 color = VARIANT_COLORS[i_variant]
@@ -126,42 +127,121 @@ if PLOT_METRICS:
             
 #for i_scenario, scenario in enumerate(scenario_names)
 
-# print('Plotting deterministic model time series examples...')
+def run_one_det_sim(model_name, params_dict, scenario, i_var):
+    print(f'Simulating {model_name} with '
+          f' {params_dict} in variant #{i_var+1} scenario {scenario.name}...')
+    return sc_fitting.construct_model_and_simulate_scenario(
+        model_name, params_dict, scenario, i_variation=i_var, 
+        apply_stop_criteria=False, zero_acc_after_exit=False)
 
-# DET_MODEL_NAMES = ('', 'oBEo', 'oVAoBEvoAI')   
+print('Plotting deterministic model time series examples...')
+
+# the models to show simulations for
+DET_MODEL_NAMES = ('', 'oBEo', 'oVAoBEvoAI')
+DET_MODEL_BASES = (0, 0, 1)
+DET_MODEL_VARIANTS = (0, 1, 2)
+# criterion to use for selecting a parameterisation to show for each model   
+MODEL_FOCUS_CRITS = ('Yield acceptance hesitation', 'Gap acceptance hesitation', 
+                     'Priority assertion')
+DEFAULT_FOCUS_MODEL = 'oVAoBEvoAI'
          
-# # get simulation results, by loading existing, or looping through models and simulating
-# if sc_fitting.results_exist(DET_SIM_RESULTS_FNAME) and not OVERWRITE_SAVED_SIM_RESULTS:
-#     sims = sc_fitting.load_results(DET_SIM_RESULTS_FNAME)
-# else:
-#     sims = {}
-#     for model_name in DET_MODEL_NAMES:
-#         det_fit = det_fits[model_name]
+# get simulation results, by loading existing, or looping through models and simulating
+if sc_fitting.results_exist(DET_SIM_RESULTS_FNAME) and not OVERWRITE_SAVED_DET_SIM_RESULTS:
+    det_sims = sc_fitting.load_results(DET_SIM_RESULTS_FNAME)
+else:
+    
+    # first, figure out which model parameterisation to use for each model
+    idx_plot_params = []
+    for i_model, model_name in enumerate(DET_MODEL_NAMES):
+        det_fit = det_fits[model_name]
+        idx_max_crit_params = get_max_crit_parameterisations(det_fit)
+        crit_details = det_fit.crit_details[MODEL_FOCUS_CRITS[i_model]]
+        max_crit_metric_vals = crit_details.metric_values[idx_max_crit_params, :]
+        if crit_details.crit_greater_than:
+            best_metric_val_per_param = np.nanmax(max_crit_metric_vals, axis=1)
+            best_metric_val = np.nanmax(best_metric_val_per_param)
+        else:
+            best_metric_val_per_param = np.nanmin(max_crit_metric_vals, axis=1)
+            best_metric_val = np.nanmin(best_metric_val_per_param)
+        idx_best_among_max_crit = np.nonzero(
+            best_metric_val_per_param == best_metric_val)[0][0]
+        idx_plot_params.append(idx_max_crit_params[idx_best_among_max_crit])
+        
+    # then, figure out which kinematic variant to use for each scenario
+    i_plot_kinem_vars = []
+    for i_scenario, scenario_name in enumerate(SCENARIO_NAMES):
+        scen_crit = SCENARIO_CRITERIA[i_scenario]
+        if scen_crit in MODEL_FOCUS_CRITS:
+            focus_model_name = DET_MODEL_NAMES[MODEL_FOCUS_CRITS.index(scen_crit)]
+        else:
+            focus_model_name = DEFAULT_FOCUS_MODEL
+        det_fit = det_fits[focus_model_name]
+        i_focus_model = DET_MODEL_NAMES.index(focus_model_name)
+        i_plot_kinem_vars.append(
+            get_best_scen_var_for_paramet(det_fit, idx_plot_params[i_focus_model], 
+                                          scenario_name, verbose=True))
+        
+    # now simulate the models
+    det_sims = {}
+    for i_model, model_name in enumerate(DET_MODEL_NAMES):
+        det_fit = det_fits[model_name]
+        params_array = det_fit.results.params_matrix[idx_plot_params[i_model]]
+        params_dict = det_fit.get_params_dict(params_array)
+
+        # - loop through scenarios and run simulations
+        det_sims[model_name] = {}
+        for i_scenario, scenario in enumerate(SCENARIOS.values()):
+            i_var = i_plot_kinem_vars[i_scenario]
+            det_sims[model_name][scenario.name] = run_one_det_sim(
+                model_name, params_dict, scenario, i_var)
+    # save simulation results
+    sc_fitting.save_results(det_sims, DET_SIM_RESULTS_FNAME)
+    
+
+# plot
+PED_V_LIMS = (-.5, 2.5)
+V_LIMS = ((12.5, 14.5), (-1, 17), PED_V_LIMS, PED_V_LIMS, PED_V_LIMS)
+T_MAXS = (3, 7.3, 7.3, 7.3, 6)
+for i_model, model_name in enumerate(DET_MODEL_NAMES):
+    det_fit = det_fits[model_name]
+    for i_scenario, scenario_name in enumerate(SCENARIO_NAMES):
+        axs = fig_axs[3:5, i_scenario]
+        sim = det_sims[model_name][scenario_name]
+        
+        # get the active agent and set colours
+        act_agent = None
+        i_act_agent = None
+        for i_agent, agent in enumerate(sim.agents):
+            if agent.const_acc == None:
+                act_agent = agent
+                i_act_agent = i_agent
+                break
+        act_agent.plot_color = VARIANT_COLORS[DET_MODEL_VARIANTS[i_model]]
+        act_agent.other_agent.plot_color = sc_plot.COLORS['passive agent grey']
+        if i_model == 0:
+            i_plot_agents = (1-i_act_agent, i_act_agent)
+            agent_alpha = (1, 1)
+        else:
+            i_plot_agents = (i_act_agent,)
+            agent_alpha = (1,)
             
-#         # get parameterisation to simulate
+        # plot
+        sim.do_kinem_states_plot(np.insert(axs, 0, None), veh_stop_dec=False, 
+                                 agent_alpha=agent_alpha,
+                                 i_plot_agents=i_plot_agents,
+                                 axis_labels=(i_scenario==0),
+                                 plot_fill=True)
+        axs[0].set_xlim(0, T_MAXS[i_scenario])
+        axs[0].set_ylim(V_LIMS[i_scenario][0], V_LIMS[i_scenario][1])
+        axs[1].set_xlim(0, T_MAXS[i_scenario])
+        axs[1].set_ylim(-4, 17)
+        axs[1].set_xlabel('Time (s)')
+        
+
+plt.show()
 
 
-#         # - loop through scenarios and run simulations
-#         sims[model_name] = {}
-#         for scenario in SCENARIOS.values():
-#             sim_iter = ((model_name, i, n_paramets, params_dicts[i], scenario) 
-#                         for i in range(n_paramets))
-#             if PARALLEL:
-#                 sims[model_name][scenario.name] = list(
-#                     pool.starmap(run_one_sim, sim_iter)) 
-#             else:
-#                 sims[model_name][scenario.name] = list(
-#                     itertools.starmap(run_one_sim, sim_iter)) 
-#             # also store the parameterisation indices
-#             sims[model_name]['idx_paramets'] = idx_paramets
-#             # sim = sc_fitting.construct_model_and_simulate_scenario(
-#             #     model_name, params_dict, scenario, apply_stop_criteria=False,
-#             #     zero_acc_after_exit=False)
-#             #sims[model_name][scenario_name].append(sim)
-#     # save simulation results
-#     sc_fitting.save_results(sims, SIM_RESULTS_FNAME)
-
-# if SAVE_PDF:
-#     file_name = sc_plot.FIGS_FOLDER + 'fig1.pdf'
-#     print(f'Saving {file_name}...')
-#     plt.savefig(file_name, bbox_inches='tight')
+if SAVE_PDF:
+    file_name = sc_plot.FIGS_FOLDER + 'fig1.pdf'
+    print(f'Saving {file_name}...')
+    plt.savefig(file_name, bbox_inches='tight')
