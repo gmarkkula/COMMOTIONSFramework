ae9c9d39aa8a31c51ca6772adb7f4a12ad8ea140
gmarkkula <g.markkula@leeds.ac.uk>
2022-02-25 09:37:16 +0000

    Excluding non-crossing parameterisations from HIKER fits

--- SCPaper/do_12_analyse_HIKER_fits.py
+++ SCPaper/do_12_analyse_HIKER_fits.py
@@ -21,11 +21,13 @@ import matplotlib.pyplot as plt
 import parameter_search
 import sc_fitting
 
+DO_PARAMS_PLOT = True
+DO_CIT_CDF_PLOTS = True
 
-MAX_NON_CROSS_TRIALS = np.inf
-if not np.isinf(MAX_NON_CROSS_TRIALS):
-    print(f'NB: Excluding parameterisations with more than {MAX_NON_CROSS_TRIALS}'
-          ' non-crossing trials.\n')
+CIT_CDF_MAX_NON_CROSS_TRIALS = np.inf
+if not np.isinf(CIT_CDF_MAX_NON_CROSS_TRIALS):
+    print('NB: Excluding parameterisations with more than'
+          f'{CIT_CDF_MAX_NON_CROSS_TRIALS} non-crossing trials from CIT CDFs.\n')
 
 
 # find pickle files from the HIKER fits
@@ -43,44 +45,45 @@ for hiker_fit_file in hiker_fit_files:
     print(f'\tParameterisations tested: {hiker_fit.n_parameterisations}')
     n_total_params_tested += hiker_fit.n_parameterisations
     
-    # go through
-    i_included_params = []
-    n_non_cross_trials = []
-    for i_param in range(hiker_fit.n_parameterisations):
-        n_non_cross_trials.append(np.count_nonzero(np.isnan(hiker_fit.results.metrics_matrix[i_param, :, :])))
-        if n_non_cross_trials[-1] <= MAX_NON_CROSS_TRIALS:
-            i_included_params.append(i_param)
-    n_non_cross_trials = np.array(n_non_cross_trials)
-    idx_max_non_cross_trials = np.nonzero(n_non_cross_trials == np.amax(n_non_cross_trials))[0]
-    print(f'Example parameterisations with max for model of'
-          f' {np.amax(n_non_cross_trials)} non-cross trials:')
-    params_array = hiker_fit.results.params_matrix[idx_max_non_cross_trials[0]]
-    print(hiker_fit.get_params_dict(params_array))
+    idx_yielding_cits = [i for i, name in enumerate(hiker_fit.metric_names) if 'y' in name]
+    yielding_cits = hiker_fit.results.metrics_matrix[:, idx_yielding_cits, :]
+    noncross_yield_params = np.any(np.isnan(yielding_cits), axis=(1, 2))
+    cross_yield_params = ~noncross_yield_params
+    n_cross_yield_params = np.count_nonzero(cross_yield_params)
+    print(f'\tFound {n_cross_yield_params}'
+          f' ({100 * n_cross_yield_params / hiker_fit.n_parameterisations:.1f} %)'
+          ' parameterisations which always crossed in vehicle yielding scenarios.')
     
+    if DO_PARAMS_PLOT:
+        sc_fitting.do_params_plot(hiker_fit.param_names, 
+                                  hiker_fit.results.params_matrix, 
+                                  jitter=0.01, color=('r', 'g'), 
+                                  param_subsets=(noncross_yield_params, 
+                                                 cross_yield_params))
     
-    if len(i_included_params) > 0:
-        print(f'Plotting for {len(i_included_params)} included parameterisations:')
-        # reorganise data for plotting/analysis
-        model_cits = {}
-        for i_speed, veh_speed_mph in enumerate(sc_fitting.HIKER_VEH_SPEEDS_MPH):
-            for i_gap, veh_time_gap in enumerate(sc_fitting.HIKER_VEH_TIME_GAPS):
-                for i_yield, veh_yielding in enumerate((False, True)):
-                    scen_name = sc_fitting.get_hiker_scen_name(veh_speed_mph, 
-                                                               veh_time_gap, 
-                                                               veh_yielding)
-                    i_scen = hiker_fit.metric_names.index(
-                        scen_name + '_' + sc_fitting.HIKER_CIT_METRIC_NAME)
-                    model_cits[scen_name] = pd.DataFrame(columns=('i_param', 
-                                                                  'crossing_time'))
-                    for i_param in i_included_params:
-                        param_cits = pd.DataFrame(
-                            {'i_param': np.full(hiker_fit.n_repetitions, i_param),
-                             'crossing_time': hiker_fit.results.metrics_matrix[
-                                 i_param, i_scen, :]})
-                        model_cits[scen_name] = model_cits[scen_name].append(param_cits)
-        
-        
-        sc_fitting.do_hiker_cit_cdf_plot(model_cits, fig_name=hiker_fit.name)
+    if DO_CIT_CDF_PLOTS:
+        i_included_params = np.nonzero(cross_yield_params)[0]
+        if len(i_included_params) > 0:
+            print(f'Plotting for {len(i_included_params)} included parameterisations:')
+            # reorganise data for plotting/analysis
+            model_cits = {}
+            for i_speed, veh_speed_mph in enumerate(sc_fitting.HIKER_VEH_SPEEDS_MPH):
+                for i_gap, veh_time_gap in enumerate(sc_fitting.HIKER_VEH_TIME_GAPS):
+                    for i_yield, veh_yielding in enumerate((False, True)):
+                        scen_name = sc_fitting.get_hiker_scen_name(veh_speed_mph, 
+                                                                    veh_time_gap, 
+                                                                    veh_yielding)
+                        i_scen = hiker_fit.metric_names.index(
+                            scen_name + '_' + sc_fitting.HIKER_CIT_METRIC_NAME)
+                        model_cits[scen_name] = pd.DataFrame(columns=('i_param', 
+                                                                      'crossing_time'))
+                        for i_param in i_included_params:
+                            param_cits = pd.DataFrame(
+                                {'i_param': np.full(hiker_fit.n_repetitions, i_param),
+                                  'crossing_time': hiker_fit.results.metrics_matrix[
+                                      i_param, i_scen, :]})
+                            model_cits[scen_name] = model_cits[scen_name].append(param_cits)
+            sc_fitting.do_hiker_cit_cdf_plot(model_cits, fig_name=hiker_fit.name)
 
 print(f'*** Total number of parameterisations tested: {n_total_params_tested}')                 
                 
--- SCPaper/test_scpaper_scenario.py
+++ SCPaper/test_scpaper_scenario.py
@@ -23,11 +23,11 @@ import sc_scenario_helper
 import sc_fitting
 
 # set model
-MODEL = 'oBEvoAI'
-# get assumptions and default parameters for chosen model
-assumptions = sc_scenario.get_assumptions_dict_from_string(MODEL)
-params = copy.deepcopy(sc_fitting.DEFAULT_PARAMS)
-params_k = copy.deepcopy(sc_fitting.get_default_params_k(MODEL))
+MODEL = 'oVAoBEvoAIoEAoSNvoPF'
+# # get assumptions and default parameters for chosen model
+# assumptions = sc_scenario.get_assumptions_dict_from_string(MODEL)
+# params = copy.deepcopy(sc_fitting.DEFAULT_PARAMS)
+# params_k = copy.deepcopy(sc_fitting.get_default_params_k(MODEL))
 
 # set remaining model parameters
 # params.T = 0.5
@@ -36,23 +36,24 @@ params_k = copy.deepcopy(sc_fitting.get_default_params_k(MODEL))
 #params.tau_theta = 0.05
 #params.sigma_xdot = 0.1
 #sc_fitting.V_NY_REL = -2
-params.T_delta = 60
+#params.T_delta = 60
 #params.thetaDot_1 = 0.00
 #params.thetaDot_1 = 0.001
-params.beta_V = 1
+#params.beta_V = 1
 #params.T_Of = 3.7
 #params.sigma_O = 1.2
-for ctrl_type in sc_scenario_helper.CtrlType:
-    params_k[ctrl_type]._c = 0.431
-    params_k[ctrl_type]._sc = 0.02
+# for ctrl_type in sc_scenario_helper.CtrlType:
+#     params_k[ctrl_type]._c = 0.431
+#     params_k[ctrl_type]._sc = 0.02
 
 # params.T_s = 0.5
 # params.D_s = 0.5
 # params.ctrl_deltas *= 2
+params_dict = {'T_delta': 35.938136638046274, 'T': 0.5, 'DeltaV_th_rel': 0.001, 
+          'beta_V': 61.615502775833434, 'tau_theta': 0.0049999999999999994}
 
 
-
-if True:
+if False:
     
     SCENARIO = sc_fitting.ONE_AG_SCENARIOS['VehShortStop']
     # SCENARIO = sc_fitting.PROB_FIT_SCENARIOS['Encounter']
@@ -83,7 +84,7 @@ if True:
         metrics = sc_fitting.get_metrics_for_scenario(SCENARIO, sim, verbose=True)
 
 
-if False:
+if True:
     
     SCENARIOS = sc_fitting.HIKER_SCENARIOS
         
@@ -94,14 +95,18 @@ if False:
         for i_var in range(scenario.n_variations):
             print(f'\n{scenario.name} variation {i_var+1}/{scenario.n_variations}:')
             tic = time.perf_counter()
-            sim = sc_fitting.simulate_scenario(scenario, assumptions, params, params_k, 
-                                               i_variation=i_var, snapshots=(None, None),
-                                               noise_seeds=(None, None), 
-                                               apply_stop_criteria=True)
+            # sim = sc_fitting.simulate_scenario(scenario, assumptions, params, params_k, 
+            #                                    i_variation=i_var, snapshots=(None, None),
+            #                                    noise_seeds=(None, None), 
+            #                                    apply_stop_criteria=True)
+            sim = sc_fitting.construct_model_and_simulate_scenario(
+                MODEL, params_dict, scenario, i_variation=i_var, 
+                snapshots=(None, None), detailed_snapshots=False, 
+                noise_seeds=(None, None), apply_stop_criteria=True)
             toc = time.perf_counter()
             print('Initialising and running simulation took %.3f s.' % (toc - tic,))
             sim.do_plots(kinem_states=True, beh_probs=True, beh_activs=False, 
-                          action_val_ests=False, surplus_action_vals=False, looming=False,
+                          action_val_ests=False, surplus_action_vals=True, looming=False,
                           veh_stop_dec=False)
             metrics = sc_fitting.get_metrics_for_scenario(scenario, sim, verbose=True)
 
