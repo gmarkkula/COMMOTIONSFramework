4c42722ea74bf57a4885ec19027daf7a4818f22b
gmarkkula <g.markkula@leeds.ac.uk>
2022-03-04 06:09:03 +0000

    Update do_fig_probabilistic_hesitation.py

--- SCPaper/do_fig_probabilistic_hesitation.py
+++ SCPaper/do_fig_probabilistic_hesitation.py
@@ -20,6 +20,9 @@ import matplotlib.pyplot as plt
 import sc_fitting
 
 
+plt.close('all')
+
+
 OVERWRITE_SAVED_SIM_RESULTS = False
     
 
@@ -49,7 +52,7 @@ else:
                 found_model = True
                 break
         if not found_model:
-            raise Exception('Model {model_name} not foundamong retained models.')
+            raise Exception('Model {model_name} not found among retained models.')
             
         # draw retained parameterisations at random and simulate
         n_ret_paramets = ret_model.params_array.shape[0]
@@ -68,12 +71,15 @@ else:
     sc_fitting.save_results(sims, SIM_RESULTS_FNAME)
     
     
+
 # do plotting
 veh_entry_t = SCENARIO.initial_ttcas[sc_fitting.i_VEH_AGENT]
 veh_exit_t = veh_entry_t + (2 * sc_fitting.AGENT_COLL_DISTS[sc_fitting.i_VEH_AGENT]
                             / SCENARIO.initial_speeds[sc_fitting.i_VEH_AGENT])
-fig, axs = plt.subplots(nrows=2, ncols=len(MODEL_NAMES), figsize=(8, 5),
+veh_ttcss = veh_entry_t - np.arange(0, SCENARIO.end_time, SCENARIO.time_step) 
+kin_fig, kin_axs = plt.subplots(nrows=2, ncols=len(MODEL_NAMES), figsize=(8, 5),
                         sharex='col', sharey='row', tight_layout=True)
+st_fig, st_axs = plt.subplots(nrows=4, ncols=1, sharex='col', tight_layout=True)
 for i_model, model_name in enumerate(MODEL_NAMES):
     for i_sim, sim in enumerate(sims[model_name]):
         ped_agent = sim.agents[sc_fitting.i_PED_AGENT]
@@ -84,12 +90,12 @@ for i_model, model_name in enumerate(MODEL_NAMES):
         #ax.set_xlim(-0.5, 6.5)
         #ax.set_ylim(-3, 3)
         # speed
-        ax = axs[0, i_model]
+        ax = kin_axs[0, i_model]
         ax.plot(sim.time_stamps, ped_agent.trajectory.long_speed,
                              'k', alpha=ALPHA)
         ax.set_ylim(-.1, 4.1)
         # distance
-        ax = axs[1, i_model]
+        ax = kin_axs[1, i_model]
         ax.plot(sim.time_stamps, ped_agent.signed_CP_dists,
                              'k', alpha=ALPHA)
         ax.set_ylim(-ped_agent.coll_dist-1, 6)
@@ -97,8 +103,41 @@ for i_model, model_name in enumerate(MODEL_NAMES):
             ax.fill(np.array((veh_entry_t, veh_exit_t, veh_exit_t, veh_entry_t)),
                     np.array((1, 1, -1, -1)) * ped_agent.coll_dist,
                     c='red', edgecolor='none', alpha=0.3)
-      
-    axs[-1, i_model].set_xlabel('Time (s)')            
-axs[0, 0].set_ylabel('$v$ (m/s)')
-axs[1, 0].set_ylabel('$d_{CP}$ (m)')
+        
+        # states plotting
+        if 'oSNv' in model_name:
+            veh_agent = sim.agents[sc_fitting.i_VEH_AGENT]
+            perc_cs_dist = ped_agent.perception.states.x_perceived[0, :] - veh_agent.coll_dist
+            perc_speed = ped_agent.perception.states.x_perceived[1, :]
+            perc_ttcs = perc_cs_dist / perc_speed
+            ax = st_axs[0]
+            ax.plot(sim.time_stamps, perc_ttcs, 'k', alpha=ALPHA)
+            
+            i_no_action = ped_agent.i_no_action
+            i_dec_action = i_no_action-2
+            ax = st_axs[1]
+            ax.plot(sim.time_stamps, ped_agent.states.mom_action_vals[i_no_action, :], 'k', alpha=ALPHA)
+            ax = st_axs[2]
+            ax.plot(sim.time_stamps, ped_agent.states.mom_action_vals[i_dec_action, :], 'k', alpha=ALPHA)
+            ax = st_axs[3]
+            # ax.plot(sim.time_stamps, ped_agent.states.est_action_vals[i_dec_action, :]
+            #         - ped_agent.states.est_action_vals[i_no_action, :], 'k', alpha=ALPHA)
+            ax.plot(sim.time_stamps, ped_agent.states.est_action_surplus_vals[i_dec_action, :], 'k', alpha=ALPHA)
+            
+            # i_actions = get_actions_to_plot(ped_agent)
+            # for idx_action, i_action in enumerate(i_actions):
+            #     ax = st_axs[idx_action*2+1]
+            #     ax.plot(sim.time_stamps, ped_agent.states.mom_action_vals[i_action, :], 'k', alpha=ALPHA)
+            #     ax = st_axs[idx_action*2+2]
+            #     ax.plot(sim.time_stamps, ped_agent.states.est_action_vals[i_action, :], 'k', alpha=ALPHA)
+            
+    st_axs[0].plot(sim.time_stamps, veh_ttcss, 'r-', alpha=0.5)
+    kin_axs[-1, i_model].set_xlabel('Time (s)')            
+kin_axs[0, 0].set_ylabel('$v$ (m/s)')
+kin_axs[1, 0].set_ylabel('$d_{CP}$ (m)')
+st_axs[0].set_ylim(-5, 15)
+for i in (1, 2):
+    st_axs[i].set_ylim(0.2, 0.25)
+st_axs[3].set_ylim(-0.02, 0.01)
+
             
