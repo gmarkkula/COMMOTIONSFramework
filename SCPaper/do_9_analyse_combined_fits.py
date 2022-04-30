2c91dfb798e246459b4ee10faae54efdf4ae8c06
gmarkkula <g.markkula@leeds.ac.uk>
2022-04-30 07:09:38 +0100

    Added saving of Fig S16

--- SCPaper/do_9_analyse_combined_fits.py
+++ SCPaper/do_9_analyse_combined_fits.py
@@ -10,9 +10,17 @@ import do_6_analyse_probabilistic_fits
 do_6_analyse_probabilistic_fits.DO_TIME_SERIES_PLOTS = False
 do_6_analyse_probabilistic_fits.DO_PARAMS_PLOTS = False
 do_6_analyse_probabilistic_fits.DO_RETAINED_PARAMS_PLOT = False
-do_6_analyse_probabilistic_fits.DO_OUTCOME_PLOT = True
-
-do_6_analyse_probabilistic_fits.N_CRIT_FOR_TS_PLOT = 3
+do_6_analyse_probabilistic_fits.DO_CRIT_PLOT = True # supplementary figure
+do_6_analyse_probabilistic_fits.CRIT_PLOT_MODELS = (
+    'oVAoBEvoAIoSNv', 
+    'oVAoBEvoAIoEAoSNc', 'oVAoBEvoAIoEAoSNv', 
+    'oVAoBEvoAIoEAoSNvoPF',
+    'oVAoBEooBEvoAIoEAoSNvoPF',
+    'oVAaoVAloBEvoAIoEAoSNc', 'oVAaoVAloBEvoAIoEAoSNvoPF')
+do_6_analyse_probabilistic_fits.CRIT_PLOT_FIG_NO = 16
+do_6_analyse_probabilistic_fits.DO_OUTCOME_PLOT = False
+do_6_analyse_probabilistic_fits.SAVE_FIGS = True
+do_6_analyse_probabilistic_fits.RET_PARAMS_PLOTS_TO_SAVE = ()
 
 # same analysis as do_6.., just using different input and output files 
 comb_fits = do_6_analyse_probabilistic_fits.do(sc_fitting.COMB_FIT_FILE_NAME_FMT, 
