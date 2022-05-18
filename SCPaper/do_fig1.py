1e3486628626cd9e45ee8ab19bf571e98f9c5d64
gmarkkula <g.markkula@leeds.ac.uk>
2022-05-18 15:26:09 +0100

    Minor adjustments

--- SCPaper/do_fig1.py
+++ SCPaper/do_fig1.py
@@ -29,7 +29,7 @@ PLOT_DET_EXAMPLES = True
 
 OVERWRITE_SAVED_DET_SIM_RESULTS = False
 
-SAVE_PDF = True
+SAVE_PDF = False
 if SAVE_PDF:
     SCALE_DPI = 1
 else:
@@ -42,7 +42,7 @@ N_ROWS = 5
 SCENARIO_CRITERIA = ('Priority assertion', 'Short-stopping', 'Gap acceptance hesitation',
           'Yield acceptance hesitation', 'Early yield acceptance')
 SCENARIO_METRIC_NAMES = ('$\overline{v}_\mathrm{v}/v_\mathrm{v,free}$ (-)', 
-                         '$\overline{d}/d_\mathrm{stop}$ (-)',
+                         '$\overline{d - d_\mathrm{stop}}$ (m/s²)',
                          '$\overline{v}_\mathrm{p}/v_\mathrm{p,free}$ (-)',
                          '$\overline{v}_\mathrm{p}/v_\mathrm{p,free}$ (-)', 
                          '$v_\mathrm{v}(t_\mathrm{cross})$ (m/s)')
@@ -58,7 +58,7 @@ rng = default_rng(0)
 plt.close('all')
 fig, fig_axs = plt.subplots(num=1, nrows = N_ROWS, ncols = N_COLS, 
                         figsize=(sc_plot.FULL_WIDTH, 
-                                 0.8*sc_plot.FULL_WIDTH),
+                                 0.7*sc_plot.FULL_WIDTH),
                         dpi=sc_plot.DPI * SCALE_DPI)
 
 ILL_PAD = 0.02
@@ -74,7 +74,7 @@ if PLOT_ILLUSTRATIONS:
         ax.axis('off')
         ax.set_title(SCENARIO_CRITERIA[i_scenario] + '\n', 
                      fontsize=sc_plot.DEFAULT_FONT_SIZE)
-        ax_x = ILL_PAD + i_scenario * ILL_W
+        ax_x = i_scenario * ILL_W
         ax_y = 0.76
         ax_w = ILL_W
         ax_h = ILL_W
@@ -92,24 +92,24 @@ with open(os.devnull, 'w') as devnull:
     
 CRIT_BASES = ('', 'oVA')
 BASE_DASHES = ((6,1), (1,0))
-BASE_NAMES = ('Snapshot payoffs', 'Affordances')
+BASE_NAMES = ('Short-term payoffs', 'Affordance-based')
 CRIT_VARIANTS = ('', 'oBEo', 'oBEvoAI')
 VARIANT_COLORS = (sc_plot.COLORS['base variant black'],
                   sc_plot.COLORS['oBEo variant yellow'],
                   'green')
-VARIANT_NAMES = ('None', 'Observation', 'Value/action')
+VARIANT_NAMES = ('None', 'Observation-based', 'Value-based (action-sensitive)')
     
-PANEL_MID_XS = ILL_PAD + np.arange(len(SCENARIOS)) * ILL_W + ILL_W / 2
+PANEL_MID_XS = np.arange(len(SCENARIOS)) * ILL_W + ILL_W / 2 
     
 if PLOT_METRICS:
     
     print('Plotting metric distributions...')
     
     PANEL_W = 0.8 * ILL_W
-    PANEL_H = 0.12
+    PANEL_H = 0.15
     
     def transf_kde(x, kde, base_y, xlims):
-        SCALE = 5
+        SCALE = 7
         THRESH = 1e-3
         # get KDE PDF and scale so that the area in plot coordinates is the same
         # between panels
@@ -129,7 +129,7 @@ if PLOT_METRICS:
         for i_base, base in enumerate(CRIT_BASES):
             ax = fig_axs[i_base + 1, i_scenario]
             ax_x = PANEL_MID_XS[i_scenario] - PANEL_W / 2
-            ax_y = 0.59 - i_base * 0.13
+            ax_y = 0.55 - i_base * 0.16
             ax.set_position([ax_x, ax_y, PANEL_W, PANEL_H])
             for i_variant, variant in enumerate(CRIT_VARIANTS):
                 base_y = len(CRIT_VARIANTS) - i_variant
@@ -174,7 +174,7 @@ if PLOT_METRICS:
     sc_plot.add_panel_label('B', (PANEL_LABEL_X, 0.7))
             
             
-#for i_scenario, scenario in enumerate(scenario_names)
+
 
 def run_one_det_sim(model_name, params_dict, scenario, i_var):
     print(f'Simulating {model_name} with '
@@ -183,149 +183,152 @@ def run_one_det_sim(model_name, params_dict, scenario, i_var):
         model_name, params_dict, scenario, i_variation=i_var, 
         apply_stop_criteria=False, zero_acc_after_exit=False)
 
-print('Plotting deterministic model time series examples...')
 
-# the models to show simulations for
-DET_MODEL_NAMES = ('oBEo', '', 'oVAoBEvoAI')
-DET_MODEL_BASES = (0, 0, 1)
-DET_MODEL_VARIANTS = (1, 0, 2)
-# criterion to use for selecting a parameterisation to show for each model   
-MODEL_FOCUS_CRITS = ('Yield acceptance hesitation', 'Gap acceptance hesitation', 
-                     'Priority assertion')
-DEFAULT_FOCUS_MODEL = 'oVAoBEvoAI'
-         
-# get simulation results, by loading existing, or looping through models and simulating
-if sc_fitting.results_exist(DET_SIM_RESULTS_FNAME) and not OVERWRITE_SAVED_DET_SIM_RESULTS:
-    det_sims = sc_fitting.load_results(DET_SIM_RESULTS_FNAME)
-else:
+if PLOT_DET_EXAMPLES:
+
+    print('Plotting deterministic model time series examples...')
     
-    # first, figure out which model parameterisation to use for each model
-    idx_plot_params = []
+    # the models to show simulations for
+    DET_MODEL_NAMES = ('oBEo', '', 'oVAoBEvoAI')
+    DET_MODEL_BASES = (0, 0, 1)
+    DET_MODEL_VARIANTS = (1, 0, 2)
+    # criterion to use for selecting a parameterisation to show for each model   
+    MODEL_FOCUS_CRITS = ('Yield acceptance hesitation', 'Gap acceptance hesitation', 
+                         'Priority assertion')
+    DEFAULT_FOCUS_MODEL = 'oVAoBEvoAI'
+             
+    # get simulation results, by loading existing, or looping through models and simulating
+    if sc_fitting.results_exist(DET_SIM_RESULTS_FNAME) and not OVERWRITE_SAVED_DET_SIM_RESULTS:
+        det_sims = sc_fitting.load_results(DET_SIM_RESULTS_FNAME)
+    else:
+        
+        # first, figure out which model parameterisation to use for each model
+        idx_plot_params = []
+        for i_model, model_name in enumerate(DET_MODEL_NAMES):
+            det_fit = det_fits[model_name]
+            idx_max_crit_params = get_max_crit_parameterisations(det_fit)
+            idx_best_params_for_focus_crit = get_best_parameterisations_for_crit(
+                det_fit, MODEL_FOCUS_CRITS[i_model], 
+                idx_params_subset=idx_max_crit_params)
+            print(model_name)
+            print(idx_best_params_for_focus_crit)
+            if model_name == '':
+                idx_plot_params.append(idx_max_crit_params[0])
+            else:
+                idx_plot_params.append(idx_best_params_for_focus_crit[0])
+            
+        # then, figure out which kinematic variant to use for each scenario
+        i_plot_kinem_vars = []
+        for i_scenario, scenario_name in enumerate(SCENARIO_NAMES):
+            scen_crit = SCENARIO_CRITERIA[i_scenario]
+            if scen_crit in MODEL_FOCUS_CRITS:
+                focus_model_name = DET_MODEL_NAMES[MODEL_FOCUS_CRITS.index(scen_crit)]
+            else:
+                focus_model_name = DEFAULT_FOCUS_MODEL
+            det_fit = det_fits[focus_model_name]
+            i_focus_model = DET_MODEL_NAMES.index(focus_model_name)
+            i_plot_kinem_vars.append(
+                get_best_scen_var_for_paramet(det_fit, idx_plot_params[i_focus_model], 
+                                              scenario_name, verbose=True))
+            
+        # now simulate the models
+        det_sims = {}
+        for i_model, model_name in enumerate(DET_MODEL_NAMES):
+            det_fit = det_fits[model_name]
+            params_array = det_fit.results.params_matrix[idx_plot_params[i_model]]
+            params_dict = det_fit.get_params_dict(params_array)
+    
+            # - loop through scenarios and run simulations
+            det_sims[model_name] = {}
+            for i_scenario, scenario in enumerate(SCENARIOS.values()):
+                i_var = i_plot_kinem_vars[i_scenario]
+                # extend simulation time, and avoid having the agent stop at goal
+                scenario.end_time =  12
+                sc_fitting.AGENT_GOALS = np.array([[0, 50], [-500, 0]]) # m
+                # simulate
+                det_sims[model_name][scenario.name] = run_one_det_sim(
+                    model_name, params_dict, scenario, i_var)
+        # save simulation results
+        sc_fitting.save_results(det_sims, DET_SIM_RESULTS_FNAME)
+        
+    
+    # plot
+    PED_V_LIMS = (-.5, 2.5)
+    V_LIMS = ((12.5, 14.5), (-1, 17), PED_V_LIMS, PED_V_LIMS, PED_V_LIMS)
+    T_MAXS = (3.5, 10.5, 9.5, 10.5, 6.5)
+    PANEL_W = 0.65 * ILL_W
+    PANEL_H = 0.09
     for i_model, model_name in enumerate(DET_MODEL_NAMES):
         det_fit = det_fits[model_name]
-        idx_max_crit_params = get_max_crit_parameterisations(det_fit)
-        idx_best_params_for_focus_crit = get_best_parameterisations_for_crit(
-            det_fit, MODEL_FOCUS_CRITS[i_model], 
-            idx_params_subset=idx_max_crit_params)
-        print(model_name)
-        print(idx_best_params_for_focus_crit)
-        if model_name == '':
-            idx_plot_params.append(idx_max_crit_params[0])
-        else:
-            idx_plot_params.append(idx_best_params_for_focus_crit[0])
-        
-    # then, figure out which kinematic variant to use for each scenario
-    i_plot_kinem_vars = []
-    for i_scenario, scenario_name in enumerate(SCENARIO_NAMES):
-        scen_crit = SCENARIO_CRITERIA[i_scenario]
-        if scen_crit in MODEL_FOCUS_CRITS:
-            focus_model_name = DET_MODEL_NAMES[MODEL_FOCUS_CRITS.index(scen_crit)]
-        else:
-            focus_model_name = DEFAULT_FOCUS_MODEL
-        det_fit = det_fits[focus_model_name]
-        i_focus_model = DET_MODEL_NAMES.index(focus_model_name)
-        i_plot_kinem_vars.append(
-            get_best_scen_var_for_paramet(det_fit, idx_plot_params[i_focus_model], 
-                                          scenario_name, verbose=True))
-        
-    # now simulate the models
-    det_sims = {}
-    for i_model, model_name in enumerate(DET_MODEL_NAMES):
-        det_fit = det_fits[model_name]
-        params_array = det_fit.results.params_matrix[idx_plot_params[i_model]]
-        params_dict = det_fit.get_params_dict(params_array)
-
-        # - loop through scenarios and run simulations
-        det_sims[model_name] = {}
-        for i_scenario, scenario in enumerate(SCENARIOS.values()):
-            i_var = i_plot_kinem_vars[i_scenario]
-            # extend simulation time, and avoid having the agent stop at goal
-            scenario.end_time =  12
-            sc_fitting.AGENT_GOALS = np.array([[0, 50], [-500, 0]]) # m
-            # simulate
-            det_sims[model_name][scenario.name] = run_one_det_sim(
-                model_name, params_dict, scenario, i_var)
-    # save simulation results
-    sc_fitting.save_results(det_sims, DET_SIM_RESULTS_FNAME)
+        for i_scenario, scenario_name in enumerate(SCENARIO_NAMES):
+            axs = fig_axs[3:5, i_scenario]
+            sim = det_sims[model_name][scenario_name]
+            
+            # get the active agent and set colours
+            act_agent = None
+            i_act_agent = None
+            for i_agent, agent in enumerate(sim.agents):
+                if agent.const_acc == None:
+                    act_agent = agent
+                    i_act_agent = i_agent
+                    break
+            act_agent.plot_color = VARIANT_COLORS[DET_MODEL_VARIANTS[i_model]]
+            act_agent.plot_dashes = BASE_DASHES[DET_MODEL_BASES[i_model]]
+            act_agent.other_agent.plot_color = sc_plot.COLORS['passive agent grey']
+            act_agent.other_agent.plot_dashes = (1, 1)
+            if i_model == 0:
+                i_plot_agents = (1-i_act_agent, i_act_agent)
+                agent_alpha = (1, 1)
+            else:
+                i_plot_agents = (i_act_agent,)
+                agent_alpha = (1,)
+                
+            # plot
+            if i_model == 0:
+                axs[1].axhline(0, ls='--', lw=0.5, color='lightgray')
+                act_agent.plot_lw = 1.5
+            else:
+                act_agent.plot_lw = 1
+            sim.do_kinem_states_plot(np.insert(axs, 0, None), veh_stop_dec=False, 
+                                     agent_alpha=agent_alpha,
+                                     i_plot_agents=i_plot_agents,
+                                     axis_labels=False, plot_fill=True, fill_r = 1,
+                                     hlines=False)
+            axs[0].set_xlim(0, T_MAXS[i_scenario])
+            axs[0].set_ylim(V_LIMS[i_scenario][0], V_LIMS[i_scenario][1])
+            axs[1].set_xlim(0, T_MAXS[i_scenario])
+            axs[1].set_ylim(-4, 17)
+            axs[1].set_xlabel('Time (s)')
+            if i_scenario == 0:
+                axs[0].set_ylabel('Speed\n(m/s)\n\n\n', va='center')
+                axs[1].set_ylabel('Position\n(m)\n\n\n', va='center')
+            ax_x = PANEL_MID_XS[i_scenario] - PANEL_W / 2
+            for i in range(2):
+                ax_y = 0.18 - i * 0.11
+                ax = axs[i]
+                ax.set_position([ax_x, ax_y, PANEL_W, PANEL_H])
+                ax.get_xaxis().set_visible(False)
+                ax.spines['bottom'].set_visible(False)
+                ax.spines['right'].set_visible(False)
+                ax.spines['top'].set_visible(False)
+                #print(axs[i].get_position())
+            
+            if i_model == 0:
+                # add a separate time axis
+                ax_y = 0.05
+                
+                t_ax = fig.add_subplot(sharex=axs[1])
+                t_ax.set_position([ax_x, ax_y, PANEL_W, 0.01])
+                t_ax.get_yaxis().set_visible(False)
+                t_ax.spines['left'].set_visible(False)
+                t_ax.spines['right'].set_visible(False)
+                t_ax.spines['top'].set_visible(False)
+                t_ax.set_ylabel('__')
+                t_ax.set_xlabel('Time (s)')
+            
     
-
-# plot
-PED_V_LIMS = (-.5, 2.5)
-V_LIMS = ((12.5, 14.5), (-1, 17), PED_V_LIMS, PED_V_LIMS, PED_V_LIMS)
-T_MAXS = (3.5, 10.5, 9.5, 10.5, 6.5)
-PANEL_W = 0.7 * ILL_W
-PANEL_H = 0.11
-for i_model, model_name in enumerate(DET_MODEL_NAMES):
-    det_fit = det_fits[model_name]
-    for i_scenario, scenario_name in enumerate(SCENARIO_NAMES):
-        axs = fig_axs[3:5, i_scenario]
-        sim = det_sims[model_name][scenario_name]
-        
-        # get the active agent and set colours
-        act_agent = None
-        i_act_agent = None
-        for i_agent, agent in enumerate(sim.agents):
-            if agent.const_acc == None:
-                act_agent = agent
-                i_act_agent = i_agent
-                break
-        act_agent.plot_color = VARIANT_COLORS[DET_MODEL_VARIANTS[i_model]]
-        act_agent.plot_dashes = BASE_DASHES[DET_MODEL_BASES[i_model]]
-        act_agent.other_agent.plot_color = sc_plot.COLORS['passive agent grey']
-        act_agent.other_agent.plot_dashes = (1, 1)
-        if i_model == 0:
-            i_plot_agents = (1-i_act_agent, i_act_agent)
-            agent_alpha = (1, 1)
-        else:
-            i_plot_agents = (i_act_agent,)
-            agent_alpha = (1,)
-            
-        # plot
-        if i_model == 0:
-            axs[1].axhline(0, ls='--', lw=0.5, color='lightgray')
-            act_agent.plot_lw = 1.5
-        else:
-            act_agent.plot_lw = 1
-        sim.do_kinem_states_plot(np.insert(axs, 0, None), veh_stop_dec=False, 
-                                 agent_alpha=agent_alpha,
-                                 i_plot_agents=i_plot_agents,
-                                 axis_labels=False, plot_fill=True, fill_r = 1,
-                                 hlines=False)
-        axs[0].set_xlim(0, T_MAXS[i_scenario])
-        axs[0].set_ylim(V_LIMS[i_scenario][0], V_LIMS[i_scenario][1])
-        axs[1].set_xlim(0, T_MAXS[i_scenario])
-        axs[1].set_ylim(-4, 17)
-        axs[1].set_xlabel('Time (s)')
-        if i_scenario == 0:
-            axs[0].set_ylabel('$v$ (m/s)\n')
-            axs[1].set_ylabel('$d_\mathrm{CP}$ (m)\n')
-        ax_x = PANEL_MID_XS[i_scenario] - PANEL_W / 2
-        for i in range(2):
-            ax_y = 0.23 - i * 0.13
-            ax = axs[i]
-            ax.set_position([ax_x, ax_y, PANEL_W, PANEL_H])
-            ax.get_xaxis().set_visible(False)
-            ax.spines['bottom'].set_visible(False)
-            ax.spines['right'].set_visible(False)
-            ax.spines['top'].set_visible(False)
-            #print(axs[i].get_position())
-        
-        if i_model == 0:
-            # add a separate time axis
-            ax_y = 0.07
-            
-            t_ax = fig.add_subplot(sharex=axs[1])
-            t_ax.set_position([ax_x, ax_y, PANEL_W, 0.01])
-            t_ax.get_yaxis().set_visible(False)
-            t_ax.spines['left'].set_visible(False)
-            t_ax.spines['right'].set_visible(False)
-            t_ax.spines['top'].set_visible(False)
-            t_ax.set_ylabel('__')
-            t_ax.set_xlabel('Time (s)')
-        
-
-                    
-sc_plot.add_panel_label('C', (PANEL_LABEL_X, 0.36))
+                        
+    sc_plot.add_panel_label('C', (PANEL_LABEL_X, 0.3))
 
 
 # legends
@@ -346,7 +349,7 @@ for i_variant in range(3):
     line, = ax.plot((-1, -1), (-1, -1), '-', color=VARIANT_COLORS[i_variant], 
                     label=VARIANT_NAMES[i_variant])
     leg_lines.append(line)
-legend = ax.legend(handles=leg_lines, frameon=False, loc=(-0.7, 0.7), 
+legend = ax.legend(handles=leg_lines, frameon=False, loc=(-0.7, 0.8), 
           title='Behavior estimation:')
 legend._legend_box.align = 'left'
 
