e722793c33c90fee166828d1209dfc757b24c7ed
gmarkkula <g.markkula@leeds.ac.uk>
2022-05-19 08:01:29 +0100

    Shrinking and adding annotations

--- SCPaper/do_fig2.py
+++ SCPaper/do_fig2.py
@@ -17,6 +17,7 @@ import os, contextlib
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import ConnectionPatch
 import sc_scenario
 from sc_scenario_helper import AccessOrder
 import sc_fitting
@@ -31,7 +32,7 @@ PLOT_MODEL_STATES = True
 OVERWRITE_SAVED_SIM_RESULTS = False
 
 
-SAVE_PDF = True
+SAVE_PDF = False
 if SAVE_PDF:
     SCALE_DPI = 1
 else:
@@ -44,11 +45,13 @@ MODEL_DISPLAY_NAME = ('Snapshot payoffs', 'Affordance-based values')
 FOCUS_MODEL = 'oVAoBEvoAI' 
 SCENARIO = sc_fitting.ONE_AG_SCENARIOS['VehShortStop']
 CRITERION = 'Short-stopping'
+SNAPSHOT_TIME = 0.5
 SIM_RESULTS_FNAME = 'fig_2_SimResults.pkl'
 i_BEHS = (sc_scenario.i_PASS1ST, sc_scenario.i_PASS2ND)
 BEH_COLORS = (sc_plot.COLORS['Passing first green'],
               sc_plot.COLORS['Passing second red']) 
 ACTION_LINE_STYLES = ('--', '-') # no action; deceleration action
+ANN_TEXT_COL = 'gray'
 
 def get_actions_to_plot(veh_agent):
     i_no_action = veh_agent.i_no_action
@@ -93,7 +96,7 @@ else:
         params_dict = det_fit.get_params_dict(params_array)
         params_dict['T_delta'] = 10
         if model_name == FOCUS_MODEL:
-            snapshots = (None, (0.5,))
+            snapshots = (None, (SNAPSHOT_TIME,))
         else:
             snapshots = (None, None)
         print(f'Simulating {model_name} with '
@@ -113,18 +116,26 @@ plt.close('all')
 
 N_TS_ROWS = 4
 fig, axs = plt.subplots(nrows=N_TS_ROWS, ncols=len(MODEL_NAMES)+1,
-                        sharex='col', figsize=(0.9*sc_plot.FULL_WIDTH, 
-                                               0.7*sc_plot.FULL_WIDTH),
+                        sharex='col', figsize=(0.65*sc_plot.FULL_WIDTH, 
+                                               0.4*sc_plot.FULL_WIDTH),
                         dpi=sc_plot.DPI * SCALE_DPI)
+i_AH_COL = len(MODEL_NAMES)
 
 
 
 
 
 # model state time series plots
-AX_W = 0.19
-AX_H = 0.13
+AX_W = 0.18
+AX_HS = np.full(4, 0.14)
+AX_HS[2] = 0.22
+AX_HS[3] = 0.1
+AX_Y_TOP = 0.91
+AX_Y_PAD = 0.04
+def get_ax_y(i_row):
+    return AX_Y_TOP - np.sum(AX_HS[0:i_row+1]) - AX_Y_PAD * (i_row-1)
 if PLOT_MODEL_STATES:
+    drawn_vline = False
     for i_model, model_name in enumerate(MODEL_NAMES):
         sim = sims[model_name]
         veh_agent = sim.agents[i_VEH_AGENT]
@@ -136,6 +147,14 @@ if PLOT_MODEL_STATES:
             for idx_action, i_action in enumerate(i_actions):
                 for idx_beh, i_beh in enumerate(i_BEHS):
                     if i_row == 2:
+                        if i_model==1 and not drawn_vline:
+                            ax.axvline(SNAPSHOT_TIME, c='gray', lw=0.75, ls=':')
+                            drawn_vline = True
+                            con = ConnectionPatch(
+                                xyA=(SNAPSHOT_TIME,-0.05), coordsA=ax.transData, 
+                                xyB=(-12, -0.05), coordsB=axs[2, i_AH_COL].transData,
+                                color='gray', lw=0.75, arrowstyle ='<-', ls=':')
+                            fig.add_artist(con)
                         # V_a|b
                         plot_y = veh_agent.states.action_vals_given_behs[i_action, 
                                                                          i_beh, :]
@@ -164,38 +183,42 @@ if PLOT_MODEL_STATES:
                             ls=ACTION_LINE_STYLES[idx_action],
                             color=BEH_COLORS[idx_beh])
                     if i_model == 0:
-                        ax.set_ylabel(ylabel + '\n')
+                        ax.set_ylabel(ylabel + '\n\n', va='center')
                     sc_plot.leave_only_yaxis(ax)
-                    ax_y = 0.72 - 0.18 * i_row
-                    ax.set_position([ax_x, ax_y, AX_W, AX_H])
+                    ax_y = get_ax_y(i_row)
+                    ax.set_position([ax_x, ax_y, AX_W, AX_HS[i_row]])
         # plot acceleration
         ax = axs[3, i_model]
         ax.plot(sim.time_stamps, veh_agent.get_stop_accs(), lw=1, 
                 ls=':', color='k')
         ax.plot(sim.time_stamps, veh_agent.trajectory.long_acc, lw=1, 
                 ls='-', color='k')
-        ax.set_ylim(-5.5, 0.1)
+        ax.set_ylim(-5.5, 0.5)
         if i_model == 0:
-            ax.set_ylabel('Acceleration (m/s$^2$)\n')
+            ax.set_ylabel('Acc. (m/s$^2$)\n\n', va='center')
+        else:
+            ax.annotate('Yield', xy=(0.7, -0.1), color=ANN_TEXT_COL)
+            ax.annotate('Model', xy=(2.5, -4), color=ANN_TEXT_COL)
         sc_plot.leave_only_yaxis(ax)
-        ax_y = 0.72 - 0.18 * 3
-        ax.set_position([ax_x, ax_y, AX_W, AX_H])
+        AX_H_DIFF = 0.06
+        ax_y = get_ax_y(3)
+        ax.set_position([ax_x, ax_y, AX_W, AX_HS[3]])
         # add a separate time axis
         sc_plot.add_linked_time_axis(ax, nudge_down=0.04)
                     
-sc_plot.add_panel_label('A', (0.05, 0.90))
-sc_plot.add_panel_label('B', (0.31, 0.90))
+sc_plot.add_panel_label('A', (0.05, 0.92))
+sc_plot.add_panel_label('B', (0.31, 0.92))
 
     
 # anticipation horizon plots
 N_AH_ROWS = 3
-i_AH_COL = len(MODEL_NAMES)
 sim = sims[FOCUS_MODEL]
 veh_agent = sim.agents[i_VEH_AGENT]
 i_actions = get_actions_to_plot(veh_agent)
 AX_X = 0.68
 AX_W = 0.26
-AX_H = 0.14
+AX_H = 0.18
+AX_Y_DIST = 0.2
 for i_row in range(N_AH_ROWS):
     for idx_action, i_action in enumerate(i_actions):
         for idx_beh, i_beh in enumerate(i_BEHS):
@@ -214,8 +237,8 @@ for i_row in range(N_AH_ROWS):
                 elif i_row == 1:
                     # values
                     plot_y = acc_ord_val.details.kinematics_values
-                    ax.set_ylabel('Value rate (-)')
-                    ax.set_ylim(-0.3, 0.1)
+                    ax.set_ylabel('Reward\nrate (-)\n', va='center')
+                    ax.set_ylim(-0.25, 0.1)
                 elif i_row == 2:
                     # discounted cumulative values
                     # (inherent access value + discounted kinematics values +
@@ -225,7 +248,8 @@ for i_row in range(N_AH_ROWS):
                         acc_ord_val.details.inh_access_value)
                     #plot_y[-1] += acc_ord_val.details.post_value_discounted
                     plot_y = veh_agent.squash_value(plot_y)
-                    ax.set_ylabel('Cumulative value (-)')
+                    ax.set_ylabel('Cumulative\nvalue (-)\n', va='center')
+                    ax.set_ylim(-0.5, 0.05)
                 if access_ord == AccessOrder.EGOFIRST:
                     alpha = 0.2
                 else:
@@ -235,7 +259,7 @@ for i_row in range(N_AH_ROWS):
                                 ls=ACTION_LINE_STYLES[idx_action],
                                 color=color, alpha=alpha)
                 sc_plot.leave_only_yaxis(ax)
-                ax_y = 0.56 - i_row * 0.18
+                ax_y = 0.62 - i_row * AX_Y_DIST
                 ax.set_position([AX_X, ax_y, AX_W, AX_H])
 
 # add a separate time axis
@@ -249,22 +273,20 @@ axs[-1, -1].axis('off')
 # annotate
 ax = axs[0, 2]
 ANN_LINE_COL = 'lightgray'
-ANN_TEXT_COL = 'gray'
-ax.text(s='Passing in front of yielding pedestrian', x=2, y=16.5, color=ANN_TEXT_COL)
+ax.text(s='Pass before yielding pedestrian', x=-1.5, y=16.5, color=ANN_TEXT_COL)
 # ax.annotate('Passing in front of yielding pedestrian', xy=(8, 14), xytext=(-4, 19),
 #             arrowprops={'arrowstyle': '-', 'lw': 1, 'color': ANN_LINE_COL}, 
 #             color=ANN_TEXT_COL)
 # ax.annotate('Yielding to crossing pedestrian', xy=(10, 9), xytext=(8, 5),
 #             arrowprops={'arrowstyle': '-', 'lw': 1, 'color': ANN_LINE_COL}, 
 #             color=ANN_TEXT_COL)
-ax.text(s='Yielding to crossing pedestrian', x=8, y=5, color=ANN_TEXT_COL)
+ax.text(s='        Yield to\ncrossing pedestrian', x=8, y=5, color=ANN_TEXT_COL)
 # ax.annotate('Yielding to yielding pedestrian', xy=(10, 9), xytext=(8, 5),
 #             arrowprops={'arrowstyle': '-', 'lw': 1, 'color': ANN_LINE_COL}, 
 #             color=ANN_TEXT_COL)
-ax.text(s='Yielding to yielding pedestrian', x=2, y=-2.5, color=ANN_TEXT_COL)
+ax.text(s='Yield to yielding pedestrian', x=0.5, y=-2.5, color=ANN_TEXT_COL)
 
 # add legends
-leg_x = -.35
 # actions
 ax = axs[0, 2]
 ACTION_LABELS = ('None', 'Decelerate')
@@ -273,8 +295,8 @@ for idx_action in range(2):
     line, = ax.plot((-1, -1), (-1, -1), lw=1, ls=ACTION_LINE_STYLES[idx_action],
             color='lightgray', label=ACTION_LABELS[idx_action])
     leg_handles.append(line)
-legend = ax.legend(handles=leg_handles, frameon=False, loc=(leg_x, 2.2),
-                   title='Own speed adjustment ($a$):')
+legend = ax.legend(handles=leg_handles, frameon=False, loc=(-.45, 1.5),
+                   title='Driver action ($a$):')
 legend._legend_box.align = 'left'
 # behaviours
 ax = axs[1, 2]
@@ -284,12 +306,12 @@ for idx_beh in range(2):
     line, = ax.plot((-1, -1), (-1, -1), lw=1, ls='-',
             color=BEH_COLORS[idx_beh], label=BEH_LABELS[idx_beh])
     leg_handles.append(line)
-legend = ax.legend(handles=leg_handles, frameon=False, loc=(leg_x, 2.85),
-                   title="Pedestrian's intended behavior ($b$):")
+legend = ax.legend(handles=leg_handles, frameon=False, loc=(0.2, 2.6),
+                   title="Pedestrian behavior ($b$):")
 legend._legend_box.align = 'left'
 
 
-sc_plot.add_panel_label('C', (0.61, 0.72))
+sc_plot.add_panel_label('C', (0.61, 0.76))
 
 
 if SAVE_PDF:
